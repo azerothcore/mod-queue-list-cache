34e09addf977c555224351c51b2e761b24ff3ce1
laasker <79991768+laasker@users.noreply.github.com>
2024-09-15 03:26:17 -0300

    .

--- src/QueueListNPC.cpp
+++ src/QueueListNPC.cpp
@@ -12,9 +12,9 @@ queue_list_npc::queue_list_npc() : CreatureScript("queue_list_npc") { }
 
 bool queue_list_npc::OnGossipHello(Player* player, Creature* creature)
 {
-    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_arena_2v2_7:25|t Show Rated Arena queues", GOSSIP_SENDER_MAIN, 1);
-    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_arena_2v2_2:25|t Show Skirmish Arena queues", GOSSIP_SENDER_MAIN, 2);
-    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_bg_killxenemies_generalsroom:25|t Show BG queues", GOSSIP_SENDER_MAIN, 3);
+    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_arena_2v2_7:25|t Show Rated Arena queues", GOSSIP_SENDER_MAIN, Show_RatedArena_Queues);
+    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_arena_2v2_2:25|t Show Skirmish Arena queues", GOSSIP_SENDER_MAIN, Show_SkirmishArena_Queues);
+    AddGossipItemFor(player, GOSSIP_ICON_BATTLE, "|TInterface\\icons\\Achievement_bg_killxenemies_generalsroom:25|t Show BG queues", GOSSIP_SENDER_MAIN, Show_Battleground_Queues);
 
     player->PlayerTalkClass->SendGossipMenu(1, creature->GetGUID());
     return true;
