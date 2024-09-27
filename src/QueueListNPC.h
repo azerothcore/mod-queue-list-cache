53fa27f48ee2903a9b505a37d59f35a73199607a
Helias <stefanoborzi32@gmail.com>
2024-09-27 17:37:36 +0200

    feat: add Help gossip option

--- src/QueueListNPC.h
+++ src/QueueListNPC.h
@@ -7,11 +7,15 @@
 #include "ScriptMgr.h"
 #include "ScriptedGossip.h"
 
+#define NPC_TEXT_ENTRY_QUEUE_LIST 93081
+
 enum Gossips : uint8
 {
+    SHOW_MAIN_MENU = 0,
     SHOW_RATED_ARENA_QUEUES = 1,
     SHOW_SKIRMISH_ARENA_QUEUES = 2,
-    SHOW_BATTLEGROUND_QUEUES = 3
+    SHOW_BATTLEGROUND_QUEUES = 3,
+    ACTION_HELP = 4
 };
 
 class QueueListCache_Npc : public CreatureScript
